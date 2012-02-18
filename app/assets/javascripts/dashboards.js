ef940a20769f16a98fb6e0926dec1af1b563d140
David Chadwick <david.chadwick7@gmail.com>
2012-02-18 00:43:33 +0000

    Added add button to search on dashboard

--- app/assets/javascripts/dashboards.js
+++ app/assets/javascripts/dashboards.js
@@ -1,18 +1,34 @@
+
+var tags = (function () {
+    var json = null;
+    $.ajax({
+        'async': false,
+        'global': false,
+        'url':"../contacts/autocomplete" ,
+        'dataType': "json",
+        'success': function (data) {
+            json = data;
+        }
+    });
+    return json;
+})(); 
+ContactArray = $.map(tags, function(i) { return i.label; });
 $(function() {
   $('#autocomplete').autocomplete({
-    source: "/contacts/autocomplete",
+   source: function (request, response) {
+        var results = $.ui.autocomplete.filter(ContactArray, request.term);
+
+        if (!results.length) {
+            $("#searchbutton").attr('value','Add');
+            $("#search_form").attr('action','/contacts/new');
+        } else {
+           $("#searchbutton").attr('value','Search');
+            $("#search_form").attr('action','/contacts');
+
+        }
+        response(results);
+   }
+    ,
     delay: 100,
-    minLength: 2,
-    select: function (event, ui) {
-		$(event.target).val(ui.item.id);
-		window.location = "/contacts/" + ui.item.id;
-		return false;
-	}
-  });
-});
-
-
-
-
-
-
+    minLength: 2
+  });});
