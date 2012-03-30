bab8a84832defc6dbc38c7842cc5345baa4692a6
Oliver Chesterman <oliverchesterman@hotmail.com>
2012-03-30 12:21:36 +0100

    Adding devise layout for sign-in/sign up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,16 @@
 require 'backbone_helpers'
 
 class ApplicationController < ActionController::Base
+  
+  layout :layout_by_resource
+
+  def layout_by_resource
+    if devise_controller?
+      "sign"
+    else
+      "application"
+    end
+  end
 
   protect_from_forgery
   before_filter :authenticate_user!
